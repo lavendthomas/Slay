e28b6ec089468299b2bdd3098340342df1d9e255
Thomas Lavend'Homme <lavendthomas@posteo.net>
2018-10-27 18:35:34 +0200

    Initial Commit

---
+++ desktop/src/be/ac/umons/slay/g02/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package be.ac.umons.slay.g02.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import be.ac.umons.slay.g02.Slay;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Slay(), config);
+	}
+}
