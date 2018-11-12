3b117ff8c3e7916bfa7c93182d9df5fcd51e9767
Thomas Lavend'Homme <lavendthomas@posteo.net>
2018-11-12 11:43:45 +0100

    Cleaned branch

--- android/assets/levels/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="tileset" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="tileset.png" width="128" height="96"/>
-</tileset>
