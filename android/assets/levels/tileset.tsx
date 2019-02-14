2ba21bcb238ee66ed129af750b87ee4ee6779917
FlorianiL <39127199+FlorianiL@users.noreply.github.com>
2019-02-14 16:43:58 +0100

    add maps tmx and xml

--- android/assets/levels/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="TileSet" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="TileSet.png" width="256" height="256"/>
-</tileset>
--- core/src/be/ac/umons/slay/g02/Slay.java
+++ core/src/be/ac/umons/slay/g02/Slay.java
@@ -6,51 +6,38 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
 
 
 public class Slay extends ApplicationAdapter {
-	private TiledMap map;
-	private TiledMapRenderer renderer;
-	OrthographicCamera camera;
 
-	
-	@Override
-	public void create () {
+    private TiledMap map;
+    private HexagonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
 
-	    map = new TmxMapLoader().load("levels/level1.tmx");
 
-		renderer = new HexagonalTiledMapRenderer(map, .9f);
+    @Override
+    public void create() {
+        map = new TmxMapLoader().load("worlds/g02_02.tmx");
 
-		//Source : http://www.pixnbgames.com/blog/libgdx/how-to-use-libgdx-tiled-drawing-with-libgdx/
-		MapProperties properties = map.getProperties(); //Récupère propriétés de la carte
+        renderer = new HexagonalTiledMapRenderer(map, 1);
 
-		int tileWidth         = properties.get("tilewidth", Integer.class);
-		int tileHeight        = properties.get("tileheight", Integer.class);
+        MapProperties prop = map.getProperties();
+        int w = prop.get("width", Integer.class);
+        int h = prop.get ("height", Integer.class);
 
-        int mapWidthInTiles   = properties.get("width", Integer.class);
-        int mapHeightInTiles  = properties.get("height", Integer.class);
-
-        int width = mapWidthInTiles * tileWidth;
-        int height = mapHeightInTiles * tileHeight;
-
-		camera = new OrthographicCamera(width, height);
-        camera.position.set(width/2, height/2, 0);
+        camera = new OrthographicCamera(w * 64, h * 64);
+        camera.position.set((w * 64)/2, (h * 64)/2, 0);
         camera.update();
-
-
-	}
+    }
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//camera.update();
 		renderer.setView(camera);
 		renderer.render();
-
 	}
 
 	@Override
