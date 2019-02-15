7a4060a07bec201b3e50d696efc48491d7e8299f
FlorianiL <39127199+FlorianiL@users.noreply.github.com>
2019-02-15 17:14:39 +0100

    fix error on map 01

--- core/src/be/ac/umons/slay/g02/Slay.java
+++ core/src/be/ac/umons/slay/g02/Slay.java
@@ -1,31 +1,47 @@
 package be.ac.umons.slay.g02;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSets;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
 
+import be.ac.umons.slay.g02.level.Tile;
 
-public class Slay extends ApplicationAdapter {
+
+public class Slay extends ApplicationAdapter implements ApplicationListener {
 
     private TiledMap map;
     private HexagonalTiledMapRenderer renderer;
     OrthographicCamera camera;
+    private TiledMapTileSet tileset;
 
 
     @Override
     public void create() {
-        map = new TmxMapLoader().load("worlds/g02_02.tmx");
+        map = new TmxMapLoader().load("worlds/g02_01.tmx");
 
         renderer = new HexagonalTiledMapRenderer(map, 1);
+
+        tileset = map.getTileSets().getTileSet(0);
+
 
         MapProperties prop = map.getProperties();
         int w = prop.get("width", Integer.class);
         int h = prop.get ("height", Integer.class);
+
+        float sw = Gdx.graphics.getWidth();
+        float sh = Gdx.graphics.getHeight();
+
 
         camera = new OrthographicCamera(w * 64, h * 64);
         camera.position.set((w * 64)/2, (h * 64)/2, 0);
@@ -38,13 +54,17 @@ public class Slay extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		renderer.setView(camera);
 		renderer.render();
+        TiledMapTile tile = tileset.getTile(0);
+        if (Gdx.input.justTouched()) {
+            System.out.println(Gdx.input.getX());
+            System.out.println(Gdx.input.getY());
+        }
 	}
+
 
 	@Override
 	public void dispose () {
 		map.dispose();
 	}
-
-
 
 }
--- desktop/src/be/ac/umons/slay/g02/desktop/DesktopLauncher.java
+++ desktop/src/be/ac/umons/slay/g02/desktop/DesktopLauncher.java
@@ -1,12 +1,13 @@
 package be.ac.umons.slay.g02.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import be.ac.umons.slay.g02.Slay;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Slay(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Slay(), cfg);
 	}
 }
