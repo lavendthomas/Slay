8dc8dc72c2a70e413693c593ba15d41328a5928e
FlorianiL <39127199+FlorianiL@users.noreply.github.com>
2019-02-20 18:04:54 +0100

    add coordinates calculation

--- core/src/be/ac/umons/slay/g02/gui/screens/GameScreen.java
+++ core/src/be/ac/umons/slay/g02/gui/screens/GameScreen.java
@@ -2,43 +2,35 @@ package be.ac.umons.slay.g02.gui.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.maps.tiled.AtlasTmxMapLoader;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
-import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
 
-import java.util.HashMap;
+import java.awt.Point;
 
 import be.ac.umons.slay.g02.gui.Main;
+import be.ac.umons.slay.g02.level.Coordinate;
 import be.ac.umons.slay.g02.level.Level;
 import be.ac.umons.slay.g02.level.LevelLoader;
-import be.ac.umons.slay.g02.level.Tile;
 
 import static be.ac.umons.slay.g02.gui.Main.SCREEN_HEIGHT;
 import static be.ac.umons.slay.g02.gui.Main.SCREEN_WIDTH;
 import static be.ac.umons.slay.g02.gui.Main.VIRTUAL_HEIGHT;
 import static be.ac.umons.slay.g02.gui.Main.VIRTUAL_WIDTH;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
+import static java.lang.Math.sqrt;
 
 // classe qui affiche l'interface pendant une partie
 public class GameScreen implements Screen {
@@ -51,38 +43,33 @@ public class GameScreen implements Screen {
     private HexagonalTiledMapRenderer renderer;
     OrthographicCamera camera;
 
-    int x;
-    int y;
-    int w;
-    int h;
+    private int w;
+    private int h;
+    private int tileW;
+    private int tileH;
+    private int size;
 
-    TiledMapTileLayer background;
-    TiledMapTileLayer territories;
-    TiledMapTileLayer entities;
-    TiledMapTileSet set;
+    private TiledMapTileLayer background;
+    private TiledMapTileLayer territories;
+    private TiledMapTileLayer entities;
+    private TiledMapTileSet set;
 
 
     public GameScreen(Game aGame) {
-
         game = aGame;
         stage = new Stage(new ScreenViewport());
 
         //Chargement de la map et du Level associé
-        LevelLoader.Map lvlLoader = LevelLoader.load("g02_07");
+        LevelLoader.Map lvlLoader = LevelLoader.load("g02_01");
         level = lvlLoader.getLevel();
         map = lvlLoader.getMap();
 
-        MapProperties prop = map.getProperties();
-        w = prop.get("width", Integer.class);
-        h = prop.get ("height", Integer.class);
-
-
+        // Chargement des couches et du tileset
         background = (TiledMapTileLayer) map.getLayers().get("Background");
         territories = (TiledMapTileLayer) map.getLayers().get("Territories");
         entities = (TiledMapTileLayer) map.getLayers().get("Entities");
         set = map.getTileSets().getTileSet("tileset");
 
-        renderer = new HexagonalTiledMapRenderer(map, 1);
 
         // bouton BACK
         TextButton buttonBack = new TextButton("Back",Main.skinMain);
@@ -94,11 +81,24 @@ public class GameScreen implements Screen {
                 game.setScreen(new Menu(game));
             }
         });
-        background.getCell(x,y).setTile(set.getTile(1)); // temporaire
-
         stage.addActor(buttonBack);
-        camera = new OrthographicCamera(w * 64, h * 64);
-        camera.position.set((w * 64)/2, (h * 64)/2, 0);
+
+        // Chargement de la carte dans le renderer
+        renderer = new HexagonalTiledMapRenderer(map, 1);
+
+        // Essai modification d'une tuile
+        background.getCell(0,0).setTile(set.getTile(1));
+
+        //Positionnement de la caméra selon la taille de la map
+        MapProperties prop = map.getProperties();
+        w = prop.get("width", Integer.class);
+        h = prop.get ("height", Integer.class);
+        tileW = prop.get("tilewidth", Integer.class);
+        tileH = prop.get("tileheight", Integer.class);
+        size = tileW / 2;
+
+        camera = new OrthographicCamera(w * tileW, h * tileH);
+        camera.position.set((w * tileW)/2, (h * tileH)/2, 0);
         camera.update();
 
     }
@@ -113,18 +113,19 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.setView(camera);
-        renderer.render();
         stage.draw();
         stage.act();
 
 
         //récupère position clic gauche de souris
         if (Gdx.input.justTouched()) {
-            x = Gdx.input.getX();
-            y = Gdx.input.getY();
+            int x = Gdx.input.getX();
+            int y = Gdx.input.getY();
             System.out.println("x " + x + " y " + y);
+            Hex hex = pixelToHex(x, y);
+            background.getCell(hex.q , hex.r).setTile(set.getTile(1));
         }
-
+        renderer.render();
     }
 
     @Override
@@ -157,5 +158,73 @@ public class GameScreen implements Screen {
         Main.soundButton2.dispose();
         stage.dispose();
     }
+
+
+    private Hex pixelToHex (int xp, int yp) {
+        yp = SCREEN_HEIGHT - yp;
+        int q =((2*xp)/3)/size;
+        int r = (int) ((((-xp)/3) + ((sqrt(3)/3)*yp)) / size);
+        System.out.println("q " + q + " r " + r);
+        return hex_round(new Hex(q, r));
+    }
+
+    private Hex hex_round (Hex hex) {
+        return cube_to_axial(cube_round(axial_to_cube(hex)));
+    }
+
+    private Hex cube_to_axial (Cube cube) {
+        return new Hex(cube.x, cube.z);
+    }
+
+    private  Cube axial_to_cube (Hex hex) {
+        int x = hex.q;
+        int z = hex.r;
+        int y = -x-z;
+        return new Cube(x, y, z);
+    }
+
+    private Cube cube_round (Cube cube) {
+        int rx = round(cube.x);
+        int ry = round(cube.y);
+        int rz = round(cube.z);
+
+        int x_diff = abs(rx - cube.x);
+        int y_diff = abs(ry - cube.y);
+        int z_diff = abs(rz - cube.z);
+
+        if ((x_diff > y_diff) && (x_diff > z_diff)) {
+            rx = -ry-rz;
+        }
+        else if (y_diff > z_diff) {
+            ry = -rx-rz;
+        }
+        else {
+            rz = -rx-ry;
+        }
+        return new Cube(rx, ry, rz);
+    }
+
+    private class Hex {
+        int q;
+        int r;
+
+        private Hex (int q, int r) {
+            this.q = q;
+            this.r = r;
+        }
+    }
+
+    private class Cube {
+        int x;
+        int y;
+        int z;
+
+        private Cube (int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
 }
 
