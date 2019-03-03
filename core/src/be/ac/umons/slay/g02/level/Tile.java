39f7b23a2c1029e7b0ba4f99e812328426bd5777
Thomas Lavend'Homme <lavendthomas@outlook.be>
2019-03-03 22:17:07 +0100

    Capitals management (albeit a lit buggy :/)

--- core/src/be/ac/umons/slay/g02/level/Tile.java
+++ core/src/be/ac/umons/slay/g02/level/Tile.java
@@ -29,12 +29,30 @@ public class Tile {
         return entity == null;
     }
 
-    public void setEntity(Entity entity) {
+    /**
+     *
+     * @param entity
+     * @param rec If you should check for changes in income/wages and capitals or not.
+     */
+    public void setEntity(Entity entity, boolean rec) {
         // Update the income of the territory
-        if (territory != null) {
+        if (territory != null & rec) {
             territory.update(this.entity, entity);
         }
         this.entity = entity;
+    }
+
+    public void setEntity(Entity entity) {
+        setEntity(entity, true);
+    }
+
+    /**
+     * Return true if this cell has this entity in it.
+     * @param entity
+     * @return
+     */
+    public boolean contains(Entity entity) {
+        return this.entity == entity;
     }
 
     public boolean hasSameOwner(Tile other) {
